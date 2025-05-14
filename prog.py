1d2e36172dcaa0a402e71ef28d965d7de74e4eb0
hooij804 <niels.vanhooij@kpn.com>
2025-05-14 11:28:24 +0200

    .

--- prog.py
+++ prog.py
@@ -1,5 +1,10 @@
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("echo")
+parser = argparse.ArgumentParser(description="Parse some integers.")
+parser.add_argument('num1', type=int, help='The first number to add.')
+parser.add_argument('num2', type=int, help='The second number to add.')
+parser.add_argument('-v', '--verbose', action='store_true', help='Increase output verbosity.')
 args = parser.parse_args()
-print(args.echo)
+result = args.num1 + args.num2
+print(f'The sum of {args.num1} and {args.num2} is {result}')
+if args.verbose:
+    print('Verbose mode is enabled.')
